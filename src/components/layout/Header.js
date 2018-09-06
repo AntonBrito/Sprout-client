cba57ae4fccd5f8f86cc00bc3b371af3ac2d5db7
AntonBrito <tonybrito85@gmail.com>
2018-09-06 16:43:49 -0400

    Added Prop-Types to our component,

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
+  const { branding } = props;
   return (
     <div>
-      <h1>This is my Header</h1>
+      <h1>Sprout</h1>
     </div>
   );
+};
+
+Header.protoTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
