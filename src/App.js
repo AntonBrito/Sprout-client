ca0fb4dfcd78d1b853dae026d865d8f08373a7ea
AntonBrito <tonybrito85@gmail.com>
2018-09-06 16:44:56 -0400

    Fix the error with displaying header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Post from "./components/stories/Post";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Post />
+        <Header branding="Sprout" />
+        <div>
+          <Post />
+        </div>
       </div>
     );
   }
