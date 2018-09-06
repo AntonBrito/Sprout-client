561f548f3f010483bff28d34b16234026bc85dfc
AntonBrito <tonybrito85@gmail.com>
2018-09-06 16:25:26 -0400

    clened some extra code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import PostBox from "./PostBox";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<PostBox />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
