720d786432c0e5833154b68a47324fdaac706147
AntonBrito <tonybrito85@gmail.com>
2018-09-12 13:44:49 -0400

    Organizes and clean some extra code.

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -5,9 +5,9 @@ class Post extends Component {
   render() {
     const { title, body } = this.props;
     return (
-      <div className="">
-        <h4>{title}</h4>
-        <h6>{body}</h6>
+      <div className="card card-body mb-3">
+        <h4 className="list-group-item">{title} </h4>
+        <h6 className="list-group-item">{body}</h6>
       </div>
     );
   }
