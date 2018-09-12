6141b660f3e74b245980f0b49ec279f232717d7e
AntonBrito <tonybrito85@gmail.com>
2018-09-12 12:53:36 -0400

    Passing props to my post component

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Post extends Component {
   render() {
-    return <div />;
+    const { title, body } = this.props;
+    return (
+      <div className="">
+        <h4>{title}</h4>
+        <h6>{body}</h6>
+      </div>
+    );
   }
 }
+
+Post.protoTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired
+};
 
 export default Post;
