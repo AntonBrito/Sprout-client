5d2e9bb5a65401e0668d198ac82c8022e7f37e3d
AntonBrito <tonybrito85@gmail.com>
2018-09-23 16:16:21 -0400

    Added state, Fixed some typo. Cleaned and organized some extra code

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 class Post extends Component {
   render() {
-    const { title, body } = this.props;
+    const { title, body } = this.props.post;
     return (
       <div className="card card-body mb-3">
         <h4 className="list-group-item">{title} </h4>
@@ -14,8 +14,7 @@ class Post extends Component {
 }
 
 Post.protoTypes = {
-  title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired
+  post: PropTypes.object.isRequired
 };
 
 export default Post;
--- src/components/stories/Posts.js
+++ src/components/stories/Posts.js
@@ -1,35 +1,39 @@
 import React, { Component } from "react";
+import Post from "./Post";
 
 class Posts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      Posts: [
-        {
-          title: "Miracle",
-          body: "Its amazing seeing this little one growing every single day."
-        },
-        {
-          title: "Being a father",
-          body:
-            "From the moment tha she was born, I felt in love with my new life, starting by sayim Im a father. Im her father and Im resposimble to bring her to this beatiful world."
-        },
-        {
-          title: "Raining Day",
-          body:
-            "Let the water drop take you to this beautiful ocean where everything is blue and green"
-        }
-      ]
-    };
-  }
-  return() {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Miracle",
+        body: "Its amazing seeing this little one growing every single day."
+      },
+      {
+        id: 2,
+        title: "Being a father",
+        body:
+          "From the moment tha she was born, I felt in love with my new life, starting by sayim Im a father. Im her father and Im resposimble to bring her to this beatiful world."
+      },
+      {
+        id: 3,
+        title: "Raining Day",
+        body:
+          "Let the water drop take you to this beautiful ocean where everything is blue and green"
+      }
+    ]
+  };
+
+  render() {
     const { posts } = this.state;
-    return;
-    <div>
-      {posts.map(post => (
-        <h1>{post.title}</h1>
-      ))}
-    </div>;
+
+    return (
+      <div>
+        {posts.map(post => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    );
   }
 }
 
