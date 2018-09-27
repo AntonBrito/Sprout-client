ecdb960fd92e059141f70d9e6a1b580f48ccccc6
AntonBrito <tonybrito85@gmail.com>
2018-09-27 16:47:15 -0400

    Added event clicker,

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Post extends Component {
+  state = {};
+
+  onShowClick() {
+    console.log(this.state);
+  }
+
   render() {
     const { title, body } = this.props.post;
     return (
       <div className="card card-body mb-3">
-        <h4 className="list-group-item">{title} </h4>
-        <h6 className="list-group-item">{body}</h6>
+        <h4>
+          {title}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        <h6 className="list-group-item">{body} </h6>
       </div>
     );
   }
