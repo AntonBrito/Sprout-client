0552923e6c3808e587971d935326450bac93b041
AntonBrito <tonybrito85@gmail.com>
2018-09-27 17:09:26 -0400

    Cleaned up some extra code

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Post extends Component {
-  state = {};
-
-  onShowClick() {
-    console.log(this.state);
-  }
+  state = {
+    showPostInfo: false
+  };
 
   render() {
     const { title, body } = this.props.post;
+    const { showPostInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
           {title}
-          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          <i
+            onClick={() =>
+              this.setState({ showPostInfo: !this.state.showPostInfo })
+            }
+            className="fas fa-sort-down"
+          />
         </h4>
-        <h6 className="list-group-item">{body} </h6>
+        {showPostInfo ? <h6 className="list-group-item">{body} </h6> : null}
       </div>
     );
   }
