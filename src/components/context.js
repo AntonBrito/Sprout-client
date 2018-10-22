f26b28a88b91863309996cda1f8a23771614ce25
AntonBrito <tonybrito85@gmail.com>
2018-10-22 18:40:13 -0400

    Added context component. Passing the state from Posts

---
+++ src/components/context.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+export class Provider extends Compoent {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Miracle",
+        body: "Its amazing seeing this little one growing every single day."
+      },
+      {
+        id: 2,
+        title: "Being a father",
+        body:
+          "From the moment tha she was born, I felt in love with my new life, starting by sayim Im a father. Im her father and Im resposimble to bring her to this beatiful world."
+      },
+      {
+        id: 3,
+        title: "Raining Day",
+        body:
+          "Let the water drop take you to this beautiful ocean where everything is blue and green"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
