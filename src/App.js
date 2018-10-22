0f05b78d4eb7a79e2d3b67127843cec9d79b49c4
AntonBrito <tonybrito85@gmail.com>
2018-10-22 18:45:39 -0400

    Added Provider.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import Posts from "./components/stories/Posts";
 import Header from "./components/layout/Header";
 
+import { Provider } from "./context";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header branding="Sprout" />
-        <div className="container">
-          <Posts />
+      <Provider>
+        <div className="App">
+          <Header branding="Sprout" />
+          <div className="container">
+            <Posts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
