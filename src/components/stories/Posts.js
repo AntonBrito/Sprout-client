eec6c4546bb3fd349ef42c77ee5cda46e27ba79a
AntonBrito <tonybrito85@gmail.com>
2018-10-22 18:46:25 -0400

    Passed the entire state to context.

--- src/components/stories/Posts.js
+++ src/components/stories/Posts.js
@@ -2,28 +2,6 @@ import React, { Component } from "react";
 import Post from "./Post";
 
 class Posts extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        title: "Miracle",
-        body: "Its amazing seeing this little one growing every single day."
-      },
-      {
-        id: 2,
-        title: "Being a father",
-        body:
-          "From the moment tha she was born, I felt in love with my new life, starting by sayim Im a father. Im her father and Im resposimble to bring her to this beatiful world."
-      },
-      {
-        id: 3,
-        title: "Raining Day",
-        body:
-          "Let the water drop take you to this beautiful ocean where everything is blue and green"
-      }
-    ]
-  };
-
   deletePost = id => {
     const { posts } = this.state;
 
