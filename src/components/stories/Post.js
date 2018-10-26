80daf665bbf00162934c0b3dc5a75ca339e3d427
AntonBrito <tonybrito85@gmail.com>
2018-10-26 13:47:28 -0400

    Added consumer and reorganize the component structure.

--- src/components/stories/Post.js
+++ src/components/stories/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../../context";
 
 class Post extends Component {
   state = {
@@ -13,24 +14,32 @@ class Post extends Component {
     const { showPostInfo } = this.state;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {title}
-          <i
-            onClick={() =>
-              this.setState({ showPostInfo: !this.state.showPostInfo })
-            }
-            className="fas fa-sort-down"
-            style={{ cursor: "pointer" }}
-          />
-          <i
-            className="fas fa-times"
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-            onClick={this.onDeleteClick}
-          />
-        </h4>
-        {showPostInfo ? <h6 className="list-group-item">{body} </h6> : null}
-      </div>
+      <Consumer>
+        {value => {
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {title}
+                <i
+                  onClick={() =>
+                    this.setState({ showPostInfo: !this.state.showPostInfo })
+                  }
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick}
+                />
+              </h4>
+              {showPostInfo ? (
+                <h6 className="list-group-item">{body} </h6>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
