66a356c7513235a648f4b2037faad8fe6ee1a75e
AntonBrito <tonybrito85@gmail.com>
2018-11-02 14:10:59 -0400

    Added a route for aboutPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Posts from "./components/stories/Posts";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 import AddPost from "./components/stories/AddPost";
 import { Provider } from "./context";
 
@@ -17,6 +18,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Posts} />
+                <Route exact path="/about" component={About} />
               </Switch>
             </div>
           </div>
