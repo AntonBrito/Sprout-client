0b31142168b05dcab5a8ec8f1e2843680983f2f8
AntonBrito <tonybrito85@gmail.com>
2018-11-02 14:08:01 -0400

    Added router. Created a functional component. About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Sprout</h1>
+      <p className="lead">A simple app to share your Story</p>
+      <p className="test-secondary">Version 1.0.0</p>
+    </div>
+  );
+}
