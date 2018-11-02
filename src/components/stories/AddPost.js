46e7536c3137104d86107c34fd59c8086292dd84
AntonBrito <tonybrito85@gmail.com>
2018-11-02 13:07:21 -0400

    Added errors validition to our fields required

--- src/components/stories/AddPost.js
+++ src/components/stories/AddPost.js
@@ -1,28 +1,61 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+
+import TextInputGroup from "../layout/TextInputGroup";
 import uuid from "uuid";
 
 class AddPost extends Component {
   state = {
     title: "",
-    body: ""
+    body: "",
+    errors: {}
   };
   onSubmit = (dispatch, e) => {
     e.preventDefault();
+
     const { title, body } = this.state;
+
+    // Check for errors
+    if (name === "") {
+      this.setState({ erros: { name: "Email is required" } });
+      return;
+    }
+
+    if (location === "") {
+      this.setState({ erros: { location: "location is required" } });
+      return;
+    }
+
+    if (date === "") {
+      this.setState({ erros: { date: "date is required" } });
+      return;
+    }
+
+    if (divebuddy === "") {
+      this.setState({ erros: { divebuddy: "Email is required" } });
+      return;
+    }
+
+    if (notes === "") {
+      this.setState({ erros: { notes: "notes is required" } });
+      return;
+    }
 
     const newPost = {
       id: uuid(),
       title,
       body
     };
+
     dispatch({ type: "ADD_POST", payload: newPost });
+
     // Clear State
     this.setState({
       title: "",
       body: ""
     });
   };
+
   onChange = e => this.setState({ [e.target.title]: e.target.value });
 
   render() {
@@ -37,34 +70,29 @@ class AddPost extends Component {
               <div className="card-header">Add a Story</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="title">Title</label>
-                    <input
-                      type="text"
-                      name="title"
-                      className="form-control form-control-lg"
-                      placeholder="Enter a Title"
-                      value={title}
-                      onChange={this.onChange}
-                    />
-                    <div className="form-group">
-                      <label htmlFor="story">My Story</label>
-                      <input
-                        type="text"
-                        name="story"
-                        className="form-control form-control-lg"
-                        placeholder="What would you like to share?"
-                        value={body}
-                        onChange={this.onChange}
-                      />
-                    </div>
-                    <input
-                      type="submit"
-                      value="Add a story"
-                      className="btn btn-light btn-block"
-                      cursor="pointer"
-                    />
-                  </div>
+                  <TextInputGroup
+                    label="Title"
+                    name="title"
+                    placeholder="Enter a Title"
+                    value={title}
+                    onChange={this.onChange}
+                    error={errors.title}
+                  />
+                  <TextInputGroup
+                    label="Body"
+                    type="text"
+                    name="body"
+                    placeholder="Add your Story"
+                    value={body}
+                    onChange={this.onChange}
+                    errors={errors.body}
+                  />
+                  <input
+                    type="submit"
+                    value="Add a story"
+                    className="btn btn-light btn-block"
+                    cursor="pointer"
+                  />
                 </form>
               </div>
             </div>
