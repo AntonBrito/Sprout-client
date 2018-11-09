d24fc0459e15f5f894c08324c7e79fb35578a4f9
AntonBrito <tonybrito85@gmail.com>
2018-11-09 16:38:16 -0500

    Cleared local data.

--- src/context.js
+++ src/context.js
@@ -21,25 +21,7 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: "Miracle",
-        body: "Its amazing seeing this little one growing every single day."
-      },
-      {
-        id: 2,
-        title: "Being a father",
-        body:
-          "From the moment tha she was born, I felt in love with my new life, starting by sayim Im a father. Im her father and Im resposimble to bring her to this beatiful world."
-      },
-      {
-        id: 3,
-        title: "Raining Day",
-        body:
-          "Let the water drop take you to this beautiful ocean where everything is blue and green"
-      }
-    ],
+    posts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
 
