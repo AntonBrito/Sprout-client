afebd3cd1b2f1464b0c9202cc4f128625ff43b84
AntonBrito <tonybrito85@gmail.com>
2018-11-12 23:57:48 -0500

    Added hashRouter instead of, browserRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Posts from "./components/stories/Posts";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
